f5575b0c68f23f4496d0ec2f326e4a32a3d88975
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-01-26 20:31:37 -0300

    Remoção das propridades dos componentes da parte visual

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input() image: string = "";
+  @Input() cardTitle: string = "";
+  
 }
