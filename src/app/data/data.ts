d5ab3ef31e716947bfeb987b4693c31867021ce0
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-01-27 16:18:15 -0300

    Finalização

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,123 +1,27 @@
 export const data  = [
   {
       "id": 1,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_123.png",
+      "photo": "https://blog.cronapp.io/wp-content/uploads/2020/09/javascript-1.jpg",
       "title": "Novo framework JavaScript revoluciona a forma como desenvolvemos aplicações web",
       "description": "Um novo framework JavaScript chamado Lumos foi lançado hoje, prometendo simplificar o desenvolvimento de aplicações web e oferecer um desempenho superior. Os desenvolvedores estão entusiasmados com suas características inovadoras e sua curva de aprendizado acessível."
   },
   {
       "id": 2,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_456.png",
+      "photo": "https://www.insper.edu.br/wp-content/uploads/2023/03/Logotipos-de-sistemas-operacionais-2-shutterstock_652192660.jpg.jpeg",
       "title": "Pesquisadores descobrem vulnerabilidade crítica em sistemas operacionais de última geração",
       "description": "Uma equipe de pesquisadores de segurança cibernética revelou hoje uma vulnerabilidade crítica que afeta sistemas operacionais de última geração, incluindo Windows 11, macOS Monterey e Linux 5.15. A falha pode permitir que invasores executem código malicioso remotamente."
   },
   {
       "id": 3,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_789.png",
+      "photo": "https://assets-global.website-files.com/606907b169dcd481e8fd42c4/626af17e37014a34e98e8b02_rob%C3%B4%20IA.png",
       "title": "Start-up de inteligência artificial recebe investimento milionário para desenvolver novas soluções de automação",
       "description": "Uma start-up de inteligência artificial chamada Cybervox anunciou hoje um investimento de US$ 10 milhões de uma empresa de capital de risco líder. A empresa planeja usar o financiamento para desenvolver novas soluções de automação para otimizar processos empresariais."
   },
   {
       "id": 4,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_321.png",
+      "photo": "https://www.opservices.com.br/wp-content/uploads/2017/01/Hackathon.png",
       "title": "Hackathon global reúne milhares de desenvolvedores para resolver desafios de sustentabilidade",
       "description": "Um hackathon global de 48 horas, focado em resolver desafios de sustentabilidade, atraiu mais de 10.000 desenvolvedores de todo o mundo. Os participantes colaboraram em projetos inovadores, abordando questões como energia renovável, reciclagem e conservação da água."
-  },
-  {
-      "id": 5,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_654.png",
-      "title": "Universidade renomada oferece curso online gratuito sobre aprendizado de máquina",
-      "description": "Uma universidade renomada lançou um curso online gratuito sobre aprendizado de máquina, visando capacitar estudantes e profissionais em uma das áreas de crescimento mais rápido da ciência de dados. O curso abrange fundamentos teóricos e aplicações práticas em diversas indústrias."
-  },
-  {
-      "id": 6,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_987.png",
-      "title": "Novo dispositivo IoT promete revolucionar a segurança residencial",
-      "description": "Uma start-up de tecnologia lançou um novo dispositivo IoT (Internet das Coisas) que promete revolucionar a segurança residencial. O dispositivo combina câmeras de alta definição, sensores de movimento e reconhecimento facial para oferecer proteção avançada contra intrusos."
-  },
-  {
-      "id": 7,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_135.png",
-      "title": "Empresa de software anuncia atualização importante para sua plataforma de análise de dados",
-      "description": "Uma empresa líder em software anunciou hoje uma atualização importante para sua plataforma de análise de dados, introduzindo novos recursos de visualização e ferramentas de colaboração. A atualização visa melhorar a eficiência e a usabilidade para profissionais de análise de dados."
-  },
-  {
-      "id": 8,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_246.png",
-      "title": "Pesquisadores desenvolvem algoritmo de compressão de vídeo mais eficiente",
-      "description": "Uma equipe de pesquisadores de uma universidade conceituada desenvolveu um novo algoritmo de compressão de vídeo que promete reduzir significativamente o tamanho dos arquivos sem comprometer a qualidade. O algoritmo tem potencial para melhorar a transmissão de vídeo em redes de baixa largura de banda."
-  },
-  {
-      "id": 9,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_789.png",
-      "title": "Empresa de cibersegurança revela aumento alarmante em ataques de ransomware",
-      "description": "Uma empresa de cibersegurança divulgou um relatório revelando um aumento alarmante em ataques de ransomware em todo o mundo. Os cibercriminosos estão cada vez mais direcionando organizações de todos os tamanhos, exigindo resgates significativos para descriptografar dados."
-  },
-  {
-      "id": 10,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_654.png",
-      "title": "Plataforma de e-commerce lança nova funcionalidade de recomendação personalizada",
-      "description": "Uma popular plataforma de e-commerce lançou uma nova funcionalidade de recomendação personalizada, alimentada por algoritmos de aprendizado de máquina. A funcionalidade visa melhorar a experiência do usuário, oferecendo sugestões de produtos relevantes com base no comportamento de compra."
-  },
-  {
-      "id": 11,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_321.png",
-      "title": "Pesquisadores descobrem nova vulnerabilidade em protocolo de segurança amplamente utilizado",
-      "description": "Pesquisadores de segurança cibernética revelaram uma nova vulnerabilidade em um protocolo de segurança amplamente utilizado, potencialmente afetando milhões de dispositivos em todo o mundo. A vulnerabilidade pode permitir a interceptação de comunicações seguras e a execução de ataques de phishing."
-  },
-  {
-      "id": 12,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_456.png",
-      "title": "Lançamento da nova versão do IDE mais popular recebe elogios da comunidade de desenvolvimento",
-      "description": "O lançamento da nova versão do IDE mais popular recebeu elogios da comunidade de desenvolvimento por suas melhorias de desempenho e novos recursos. Os desenvolvedores estão entusiasmados com as atualizações que visam aumentar a produtividade e facilitar o desenvolvimento de software."
-  },
-  {
-      "id": 13,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_987.png",
-      "title": "Empresa de inteligência artificial lança nova plataforma de análise de dados",
-      "description": "Uma empresa líder em inteligência artificial lançou uma nova plataforma de análise de dados, oferecendo ferramentas avançadas para extrair insights valiosos de grandes conjuntos de dados. A plataforma é projetada para atender às crescentes demandas por análise de dados em tempo real."
-  },
-  {
-      "id": 14,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_246.png",
-      "title": "Hackers divulgam detalhes de vulnerabilidade zero-day em software popular",
-      "description": "Hackers divulgaram detalhes de uma vulnerabilidade zero-day em um software popular amplamente utilizado por empresas em todo o mundo. A vulnerabilidade pode permitir que invasores executem código malicioso e comprometam sistemas sem a necessidade de autenticação."
-  },
-  {
-      "id": 15,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_123.png",
-      "title": "Estudo revela aumento na demanda por profissionais de segurança cibernética",
-      "description": "Um novo estudo revelou um aumento significativo na demanda por profissionais de segurança cibernética em todo o mundo, impulsionado por crescentes preocupações com ameaças digitais. As empresas estão buscando especialistas em segurança para proteger suas redes e dados contra ataques."
-  },
-  {
-      "id": 16,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_789.png",
-      "title": "Pesquisadores desenvolvem algoritmo de reconhecimento de voz mais preciso",
-      "description": "Uma equipe de pesquisadores desenvolveu um novo algoritmo de reconhecimento de voz que promete maior precisão e confiabilidade em ambientes ruidosos. O algoritmo tem potencial para melhorar significativamente as tecnologias de assistente virtual e reconhecimento de fala."
-  },
-  {
-      "id": 17,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_654.png",
-      "title": "Nova linguagem de programação promete simplificar o desenvolvimento de aplicativos móveis",
-      "description": "Uma nova linguagem de programação chamada Fission foi lançada hoje, prometendo simplificar o desenvolvimento de aplicativos móveis para plataformas iOS e Android. A linguagem oferece uma sintaxe intuitiva e ferramentas poderosas para desenvolvedores de aplicativos móveis."
-  },
-  {
-      "id": 18,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_321.png",
-      "title": "Start-up de realidade virtual lança plataforma de colaboração remota",
-      "description": "Uma start-up de realidade virtual lançou uma nova plataforma de colaboração remota, permitindo que equipes trabalhem juntas em ambientes virtuais imersivos. A plataforma visa melhorar a colaboração e a produtividade em um mundo cada vez mais distribuído."
-  },
-  {
-      "id": 19,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_456.png",
-      "title": "Pesquisadores desenvolvem novo método para detecção de deepfakes",
-      "description": "Pesquisadores desenvolveram um novo método para detecção de deepfakes, usando inteligência artificial para identificar manipulações em vídeos e imagens. O método tem potencial para ajudar a combater a disseminação de conteúdo falso e enganoso na internet."
-  },
-  {
-      "id": 20,
-      "photo": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_987.png",
-      "title": "Empresa de software lança nova ferramenta de gerenciamento de projetos",
-      "description": "Uma empresa de software líder lançou uma nova ferramenta de gerenciamento de projetos, projetada para simplificar o planejamento, acompanhamento e colaboração em projetos complexos. A ferramenta oferece recursos avançados de gestão de tarefas e integração com outras ferramentas populares."
   }
 ]
 
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { data } from "../../data/data";
 
 @Component({
   selector: 'app-content',
@@ -8,16 +9,26 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
+
 export class ContentComponent {
-  image: string = "https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
-  contentTitle: string = "Notícia";
-  description: string = "Olá mundo!";
+  image: string = "";
+  contentTitle: string = "";
+  description: string = "";
+  private id: string | null = "0";
 
-  constructor(private route: ActivatedRoute){
-
-  }
+  constructor(private route: ActivatedRoute){}
 
   ngOnInit(): void{
-    this.route.paramMap.subscribe(value => console.log(value))
+    this.id = this.route.snapshot.paramMap.get("id");
+
+    this.setValuesToContent(this.id);
+  }
+
+  setValuesToContent(id: string | null){
+    const result = data.filter(article => article.id === Number(this.id))[0];
+
+    this.image = result.photo;
+    this.contentTitle = result.title;
+    this.description = result.description;
   }
 }
